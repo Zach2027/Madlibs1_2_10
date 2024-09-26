e64950be4f70094e36c3067f1be563db37f6a046
Zach <zach@fake.com>
2024-09-26 09:28:27 -0700

    Scanner

--- Madlibs.java
+++ Madlibs.java
@@ -3,5 +3,17 @@ public class Madlibs{
 
     public static void main(String[] args) {
    
+        String madlib = "I wnet to the animal <noun>, the <plural noun> and the <number> beats were there.";
+        
+
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter a string");
+        String input = scanner.nextLine();
+        System.out.println("you said " + input);
+
     }
 }
