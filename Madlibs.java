dd4f455d52732b04eb0f6ffabbef04d6a2460dc1
Zach <zach@fake.com>
2024-10-06 22:29:10 -0700

    save

--- Madlibs.java
+++ Madlibs.java
@@ -4,17 +4,29 @@ public class Madlibs{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String madlib = "I wnet to the animal <noun>, the <plural_noun> and the <number> beats were there.";
-        //madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
-        //madlib = "The <adjective> <noun> was as <adjective> as the car";
-
-
-
+        String madlib = "I went to the animal <noun>, the <plural_noun> and the <number> beats were there.";
+        madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
+        madlib = "The <adjective> <noun> was as <adjective> as the car";
         
-        System.out.println("enter a Noun");
-        String noun = scanner.nextLine();
-        
-        System.out.println(madlib);
+
+        String firstMadlib = indexOf(madlib);
+        boolean firstMadlib1  = compareTo(firstMadlib, int 54);
+        System.out.print(firstMadlib1); 
+
+
+        String noun = "noun";
+        String pluralNoun = "plural noun";
+        String number = "number";
+        String adjective = "adjective";
+        String adverb = "adverb";
+
+
+
+
+        //System.out.println("enter a " + );
+        //noun = scanner.nextLine();
+        //
+        //System.out.println(madlib);
 
     }
 }
