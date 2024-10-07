3f3b4c40c5e8041b1da5579af2e3a73fd46ab3b9
Zach <zach@fake.com>
2024-10-07 12:02:15 -0700

    Done

--- Madlibs.java
+++ Madlibs.java
@@ -8,25 +8,53 @@ public class Madlibs{
         madlib = "Once upon a time in a <adjective> land, a <noun> <adverb> grew.";
         madlib = "The <adjective> <noun> was as <adjective> as the car";
         
+        int firstMadlibOpen = madlib.indexOf("<");
+        int firstMadlibClose = madlib.indexOf(">");
+        String word = madlib.substring(firstMadlibOpen + 1, firstMadlibClose);
 
-        String firstMadlib = indexOf(madlib);
-        boolean firstMadlib1  = compareTo(firstMadlib, int 54);
-        System.out.print(firstMadlib1); 
+        System.out.print("Enter a " + word);
+        String input = scanner.nextLine();
+        //replace first missing word
+        String first = madlib.substring(0, firstMadlibOpen);
+        String last = madlib.substring(firstMadlibClose + 1);
+
+        madlib = first + input + last;
+
+        //finds the next missing word
+        firstMadlibOpen = madlib.indexOf("<");
+        firstMadlibClose = madlib.indexOf(">");
+
+        word = madlib.substring(firstMadlibOpen + 1, firstMadlibClose);
+        System.out.print("enter a " + word);
+        //Takes input
+        input = scanner.nextLine();
+        //replaces the word 
+        first = madlib.substring(0, firstMadlibOpen);
+        last = madlib.substring(firstMadlibClose + 1);
+        //combines the madlib
+        madlib = first + input + last;
+
+        //Copy and past 2
+        firstMadlibOpen = madlib.indexOf("<");
+        firstMadlibClose = madlib.indexOf(">");
+        word = madlib.substring(firstMadlibOpen + 1, firstMadlibClose);
+        System.out.print("enter a " + word);
+        input = scanner.nextLine();
+        first = madlib.substring(0, firstMadlibOpen);
+        last = madlib.substring(firstMadlibClose + 1);
+
+        madlib = first + input + last;
 
 
-        String noun = "noun";
-        String pluralNoun = "plural noun";
-        String number = "number";
-        String adjective = "adjective";
-        String adverb = "adverb";
+
+
+
+        System.out.println(madlib);
 
 
 
 
-        //System.out.println("enter a " + );
-        //noun = scanner.nextLine();
-        //
-        //System.out.println(madlib);
 
+       
     }
 }
